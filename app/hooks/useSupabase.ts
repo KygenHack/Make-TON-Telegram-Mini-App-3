170b4494c11ac2aac113363520a9c4d4606213f8
KygenHack <148578932+KygenHack@users.noreply.github.com>
2024-09-21 21:54:25 +0800

    Update useSupabase.ts

--- app/hooks/useSupabase.ts
+++ app/hooks/useSupabase.ts
@@ -1,6 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xaztlrduenbmmefyihqr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhenRscmR1ZW5ibW1lZnlpaHFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzMDE3ODcsImV4cCI6MjA0MTg3Nzc4N30.Rzk-CaO0SwDYYheCbUt6LwQMGu-OtOn3F8baaGVj3j0';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
-        
+
+
+async function testSupabaseConnection() {
+    const { data, error } = await supabase.from('players').select('*');
+    
+    if (error) {
+      console.error('Supabase connection error:', error);
+    } else {
+      console.log('Supabase data:', data);
+    }
+  }
+  
+  testSupabaseConnection();
