b748750614506e6bae8e2a897dd461bb5bff3725
KygenHack <148578932+KygenHack@users.noreply.github.com>
2024-09-26 08:45:09 +0100

    update

--- app/frens/page.tsx
+++ app/frens/page.tsx
@@ -2,6 +2,8 @@
 
 import GameComponent from '@/components/GameComponent';
 import NavBar from '@/components/NavBar';
+import ReferralSystem from '@/components/ReferralSystem';
+import TaskComponent from '@/components/TaskComponent';
 import WebApp from '@twa-dev/sdk';
 import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
@@ -26,9 +28,27 @@ interface GameData {
 
 // Main component definition with a proper name
 function ReferralPage() {
-  
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [userId, setUserId] = useState('');
+  const [initData, setInitData] = useState('');
+  const [startParam, setStartParam] = useState('');
+
+  useEffect(() => {
+    const initWebApp = async () => {
+      if (typeof window !== 'undefined') {
+        const WebApp = (await import('@twa-dev/sdk')).default;
+        WebApp.ready();
+        setInitData(WebApp.initData);
+        setUserData(WebApp.initDataUnsafe.user as UserData);
+        setUserId(WebApp.initDataUnsafe.user?.id.toString() || '');
+        setStartParam(WebApp.initDataUnsafe.start_param || '');
+      }
+    };
+
+    initWebApp();
+  }, []);
+
   const NavTop = dynamic(() => import('@/components/NavTop'), { ssr: false });
-  const ReferralSystem = dynamic(() => import('@/components/ReferralSystem'), { ssr: false });
 
   return (
     <>
@@ -37,7 +57,11 @@ function ReferralPage() {
         <div className="flex-grow w-full bg-fish mt-4 rounded-t-[16px] relative top-glow z-0">
           <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[16px] p-6 carpet33">
             <div className="justify-center items-center">
-            <ReferralSystem/> 
+            <ReferralSystem
+             initData={initData}
+             userId={userId}
+             startParam={startParam}
+/> 
             </div>
           </div>
         </div>
--- components/ReferralSystem.tsx
+++ components/ReferralSystem.tsx
@@ -1,112 +1,116 @@
 import { useState, useEffect } from 'react';
 import { initUtils } from '@telegram-apps/sdk';
-import useAuth from '@/app/hooks/useAuth'; // Import the useAuth hook
 
-const ReferralSystem: React.FC = () => {
+interface ReferralSystemProps {
+  initData: string;
+  userId: string;
+  startParam: string;
+}
+
+const ReferralSystem: React.FC<ReferralSystemProps> = ({ initData, userId, startParam }) => {
   const [referrals, setReferrals] = useState<string[]>([]);
   const [referrer, setReferrer] = useState<string | null>(null);
-  const [referralCount, setReferralCount] = useState(0);
+  const [referralCount, setReferralCount] = useState(0); 
   const INVITE_URL = 'https://t.me/scorpion_world_bot/start';
 
-  // Use the useAuth hook to get the authenticated user and player data
-  const { user, playerData, isLoading } = useAuth();
-
-  // Fetch referrals only once when the component is mounted or playerData is updated
   useEffect(() => {
-    const fetchReferrals = async () => {
-      if (playerData?.id) {
+    // Calculate total referral count
+    setReferralCount(referrals.length); 
+  }, [referrals]);
+
+
+  useEffect(() => {
+    const checkReferral = async () => {
+      if (startParam && userId) {
         try {
-          const response = await fetch(`/api/referrals?userId=${playerData.id}`);
+          const response = await fetch('/api/referrals', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ userId, referrerId: startParam }),
+          });
+          if (!response.ok) throw new Error('Failed to save referral');
+        } catch (error) {
+          console.error('Error saving referral:', error);
+        }
+      }
+    };
+
+    const fetchReferrals = async () => {
+      if (userId) {
+        try {
+          const response = await fetch(`/api/referrals?userId=${userId}`);
           if (!response.ok) throw new Error('Failed to fetch referrals');
           const data = await response.json();
-
-          // Avoid duplicate referrals
           setReferrals(data.referrals);
           setReferrer(data.referrer);
-
-          // Set referral count based on unique referrals only
-          setReferralCount(new Set(data.referrals).size);
         } catch (error) {
           console.error('Error fetching referrals:', error);
         }
       }
     };
 
-    if (!isLoading && playerData?.id) {
-      fetchReferrals();
-    }
-  }, [playerData, isLoading]);
+    checkReferral();
+    fetchReferrals();
+  }, [userId, startParam]);
 
   const handleInviteFriend = () => {
     const utils = initUtils();
-    const inviteLink = `${INVITE_URL}?startapp=${playerData?.id}`;
+    const inviteLink = `${INVITE_URL}?startapp=${userId}`;
     const shareText = `Join me on this awesome Telegram mini app!`;
     const fullUrl = `https://t.me/share/url?url=${encodeURIComponent(inviteLink)}&text=${encodeURIComponent(shareText)}`;
-
-    // Ensure utils is initialized correctly
-    if (utils) {
-      utils.openTelegramLink(fullUrl);
-    } else {
-      console.error('Telegram utils not initialized properly');
-    }
+    utils.openTelegramLink(fullUrl);
   };
 
   const handleCopyLink = () => {
-    const inviteLink = `${INVITE_URL}?startapp=${playerData?.id}`;
-    navigator.clipboard.writeText(inviteLink).then(() => {
-      alert('Invite link copied to clipboard!');
-    }).catch(err => {
-      console.error('Failed to copy link: ', err);
-    });
+    const inviteLink = `${INVITE_URL}?startapp=${userId}`;
+    navigator.clipboard.writeText(inviteLink);
+    alert('Invite link copied to clipboard!');
   };
 
-  if (isLoading) {
-    return <div className='text-center'>Please Wait</div>; // Show loading state until data is fetched
-  }
-
   return (
-    <div className="text-primaryLight p-6 rounded-lg shadow-custom max-w-md mx-auto">
+    <div className="bg-white text-primaryLight p-6 rounded-lg shadow-custom max-w-md mx-auto">
       {referrer && (
-        <p className="text-green-400 text-center mb-4">You were referred by {referrer}</p>
+        <p className="text-green-400 mb-4">You were referred by user {referrer}</p>
       )}
 
       <div className="text-center">
-        <h2 className="text-3xl text-[#f48d2f] font-bold mb-2">👨‍👩‍👧‍👦 Invite Frens</h2>
-        
-        <p className="text-[#f48d2f] mb-6">
-          Invite your friends and both of you will receive bonuses. Score 10% from buddies + 2.5% from their referrals.
+      <h2 className="text-2xl text-black font-bold mb-2">👨‍👩‍👧‍👦 Invite Frens</h2>
+      <p className="text-gray-400 mb-6">
+          Invite your friends and both of you will receive bonuses.{' '}
+          <a href="#" className="text-blue-400 underline">
+            How it works?
+          </a>
         </p>
+              <p className="text-sm text-black mb-4">Score 10% from buddies + 2.5% from their referrals</p>
+        <div className="space-y-4">
+          <button
+            onClick={handleInviteFriend}
+            className="w-full py-2 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded transition duration-300"
+          >
+            Invite Friend
+          </button>
+          <button
+            onClick={handleCopyLink}
+            className="w-full py-2 bg-green-500 hover:bg-green-600 text-white font-bold rounded transition duration-300"
+          >
+            Copy Invite Link
+          </button>
+        </div>
       </div>
 
-      <div className="flex items-center justify-between mt-6">
-        <h2 className="text-lg text-[#f48d2f] font-bold mb-4">Your Frens</h2>
-        <p className="text-lg text-[#f48d2f] font-bold mb-4">Total Referrals: <strong>{referralCount}</strong></p> 
-      </div>
-
+      <div className="flex item-center justify-between mt-6">
+          <h2 className="text-lg text-black font-bold mb-4">Your Frens</h2>
+          <p className="text-lg text-black font-bold mb-4">Total Referrals: <strong>{referralCount}</strong></p> 
+        </div>
       {referrals.length > 0 && (
-        <ul>
-          {referrals.map((referral, index) => (
-            <li key={index} className="bg-gray-800 p-2 mb-2 rounded">
-              {referral}
-            </li>
-          ))}
-        </ul>
+          <ul>
+            {referrals.map((referral, index) => (
+              <li key={index} className="bg-gray-800 p-2 mb-2 rounded">
+                {referral}
+              </li>
+            ))}
+          </ul>
       )}
-
-      <div className="space-y-4">
-        <button
-          onClick={handleInviteFriend}
-          className="w-full py-2 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-full transition duration-300"
-        >
-          Invite Friend
-        </button>
-        <button
-          onClick={handleCopyLink}
-          className="w-full py-2 bg-green-500 hover:bg-green-600 text-white font-bold rounded-full transition duration-300"
-        >
-          Copy Invite Link
-        </button>
-      </div>
     </div>
   );
 };
